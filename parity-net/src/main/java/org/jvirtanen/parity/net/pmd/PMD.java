a8101fb25cca234a289b75b43cebdd4b0d3f6d96
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2014-07-30 18:44:06 +0300

    Add PMD reference implementation
    The reference implementation is based on Java NIO API and uses Nassau
    for NASDAQ MoldUDP64 1.0 support.

---
+++ parity-net/src/main/java/org/jvirtanen/parity/net/pmd/PMD.java
@@ -0,0 +1,219 @@
+package org.jvirtanen.parity.net.pmd;
+
+import static org.jvirtanen.nio.ByteBuffers.*;
+
+import java.nio.BufferOverflowException;
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+import java.nio.ReadOnlyBufferException;
+
+/**
+ * Common definitions.
+ */
+public class PMD {
+
+    private PMD() {
+    }
+
+    public static final long VERSION = 1;
+
+    public static final byte BUY  = 'B';
+    public static final byte SELL = 'S';
+
+    static final byte MESSAGE_TYPE_VERSION        = 'V';
+    static final byte MESSAGE_TYPE_SECONDS        = 'S';
+    static final byte MESSAGE_TYPE_ORDER_ADDED    = 'A';
+    static final byte MESSAGE_TYPE_ORDER_EXECUTED = 'E';
+    static final byte MESSAGE_TYPE_ORDER_CANCELED = 'X';
+    static final byte MESSAGE_TYPE_ORDER_DELETED  = 'D';
+    static final byte MESSAGE_TYPE_BROKEN_TRADE   = 'B';
+
+    /**
+     * A message.
+     */
+    public interface Message {
+
+        /**
+         * Read this message from the buffer.
+         *
+         * @param buffer a buffer
+         * @throws BufferUnderflowException if there are fewer bytes remaining
+         *   in the buffer than what this message consists of
+         */
+        void get(ByteBuffer buffer);
+
+        /**
+         * Write this message to the buffer.
+         *
+         * @param buffer a buffer
+         * @throws BufferOverflowException if there are fewer bytes remaining
+         *   in the buffer than what this message consists of
+         * @throws ReadOnlyBufferException if the buffer is read-only
+         */
+        void put(ByteBuffer buffer);
+
+    }
+
+    /**
+     * A Version message.
+     */
+    public static class Version implements Message {
+        public long version;
+
+        @Override
+        public void get(ByteBuffer buffer) {
+            version = getUnsignedInt(buffer);
+        }
+
+        @Override
+        public void put(ByteBuffer buffer) {
+            buffer.put(MESSAGE_TYPE_VERSION);
+            putUnsignedInt(buffer, version);
+        }
+    }
+
+    /**
+     * A Seconds message.
+     */
+    public static class Seconds implements Message {
+        public long second;
+
+        @Override
+        public void get(ByteBuffer buffer) {
+            second = getUnsignedInt(buffer);
+        }
+
+        @Override
+        public void put(ByteBuffer buffer) {
+            buffer.put(MESSAGE_TYPE_SECONDS);
+            putUnsignedInt(buffer, second);
+        }
+    }
+
+    /**
+     * An Order Added message.
+     */
+    public static class OrderAdded implements Message {
+        public long timestamp;
+        public long orderNumber;
+        public byte side;
+        public long instrument;
+        public long quantity;
+        public long price;
+
+        @Override
+        public void get(ByteBuffer buffer) {
+            timestamp   = getUnsignedInt(buffer);
+            orderNumber = buffer.getLong();
+            side        = buffer.get();
+            instrument  = buffer.getLong();
+            quantity    = getUnsignedInt(buffer);
+            price       = getUnsignedInt(buffer);
+        }
+
+        @Override
+        public void put(ByteBuffer buffer) {
+            buffer.put(MESSAGE_TYPE_ORDER_ADDED);
+            putUnsignedInt(buffer, timestamp);
+            buffer.putLong(orderNumber);
+            buffer.put(side);
+            buffer.putLong(instrument);
+            putUnsignedInt(buffer, quantity);
+            putUnsignedInt(buffer, price);
+        }
+    }
+
+    /**
+     * An Order Executed message.
+     */
+    public static class OrderExecuted implements Message {
+        public long timestamp;
+        public long orderNumber;
+        public long quantity;
+        public long matchNumber;
+
+        @Override
+        public void get(ByteBuffer buffer) {
+            timestamp   = getUnsignedInt(buffer);
+            orderNumber = buffer.getLong();
+            quantity    = getUnsignedInt(buffer);
+            matchNumber = getUnsignedInt(buffer);
+        }
+
+        @Override
+        public void put(ByteBuffer buffer) {
+            buffer.put(MESSAGE_TYPE_ORDER_EXECUTED);
+            putUnsignedInt(buffer, timestamp);
+            buffer.putLong(orderNumber);
+            putUnsignedInt(buffer, quantity);
+            putUnsignedInt(buffer, matchNumber);
+        }
+    }
+
+    /**
+     * An Order Canceled message.
+     */
+    public static class OrderCanceled implements Message {
+        public long timestamp;
+        public long orderNumber;
+        public long canceledQuantity;
+
+        @Override
+        public void get(ByteBuffer buffer) {
+            timestamp        = getUnsignedInt(buffer);
+            orderNumber      = buffer.get();
+            canceledQuantity = getUnsignedInt(buffer);
+        }
+
+        @Override
+        public void put(ByteBuffer buffer) {
+            buffer.put(MESSAGE_TYPE_ORDER_CANCELED);
+            putUnsignedInt(buffer, timestamp);
+            buffer.putLong(orderNumber);
+            putUnsignedInt(buffer, canceledQuantity);
+        }
+    }
+
+    /**
+     * An Order Deleted message.
+     */
+    public static class OrderDeleted implements Message {
+        public long timestamp;
+        public long orderNumber;
+
+        @Override
+        public void get(ByteBuffer buffer) {
+            timestamp   = getUnsignedInt(buffer);
+            orderNumber = buffer.getLong();
+        }
+
+        @Override
+        public void put(ByteBuffer buffer) {
+            buffer.put(MESSAGE_TYPE_ORDER_DELETED);
+            putUnsignedInt(buffer, timestamp);
+            buffer.putLong(orderNumber);
+        }
+    }
+
+    /**
+     * A Broken Trade message.
+     */
+    public static class BrokenTrade implements Message {
+        public long timestamp;
+        public long matchNumber;
+
+        @Override
+        public void get(ByteBuffer buffer) {
+            timestamp   = getUnsignedInt(buffer);
+            matchNumber = getUnsignedInt(buffer);
+        }
+
+        @Override
+        public void put(ByteBuffer buffer) {
+            buffer.put(MESSAGE_TYPE_BROKEN_TRADE);
+            putUnsignedInt(buffer, timestamp);
+            putUnsignedInt(buffer, matchNumber);
+        }
+    }
+
+}
---
+++ parity-net/src/main/java/org/jvirtanen/parity/net/pmd/PMDParser.java
@@ -0,0 +1,109 @@
+package org.jvirtanen.parity.net.pmd;
+
+import static org.jvirtanen.parity.net.pmd.PMD.*;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import org.jvirtanen.nassau.MessageListener;
+
+/**
+ * A parser for inbound messages.
+ */
+public class PMDParser implements MessageListener {
+
+    private Version       version;
+    private Seconds       seconds;
+    private OrderAdded    orderAdded;
+    private OrderExecuted orderExecuted;
+    private OrderCanceled orderCanceled;
+    private OrderDeleted  orderDeleted;
+    private BrokenTrade   brokenTrade;
+
+    private PMDListener listener;
+
+    public PMDParser(PMDListener listener) {
+        this.version       = new Version();
+        this.seconds       = new Seconds();
+        this.orderAdded    = new OrderAdded();
+        this.orderExecuted = new OrderExecuted();
+        this.orderCanceled = new OrderCanceled();
+        this.orderDeleted  = new OrderDeleted();
+        this.brokenTrade   = new BrokenTrade();
+
+        this.listener = listener;
+    }
+
+    @Override
+    public void message(ByteBuffer buffer) throws IOException {
+        byte messageType = buffer.get();
+
+        switch (messageType) {
+        case MESSAGE_TYPE_VERSION:
+            version(buffer);
+            break;
+        case MESSAGE_TYPE_SECONDS:
+            seconds(buffer);
+            break;
+        case MESSAGE_TYPE_ORDER_ADDED:
+            orderAdded(buffer);
+            break;
+        case MESSAGE_TYPE_ORDER_EXECUTED:
+            orderExecuted(buffer);
+            break;
+        case MESSAGE_TYPE_ORDER_CANCELED:
+            orderCanceled(buffer);
+            break;
+        case MESSAGE_TYPE_ORDER_DELETED:
+            orderDeleted(buffer);
+            break;
+        case MESSAGE_TYPE_BROKEN_TRADE:
+            brokenTrade(buffer);
+            break;
+        default:
+            throw new PMDException("Unknown message type: " + (char)messageType);
+        }
+    }
+
+    public void version(ByteBuffer buffer) throws IOException {
+        version.get(buffer);
+
+        listener.version(version);
+    }
+
+    private void seconds(ByteBuffer buffer) throws IOException {
+        seconds.get(buffer);
+
+        listener.seconds(seconds);
+    }
+
+    private void orderAdded(ByteBuffer buffer) throws IOException {
+        orderAdded.get(buffer);
+
+        listener.orderAdded(orderAdded);
+    }
+
+    private void orderExecuted(ByteBuffer buffer) throws IOException {
+        orderExecuted.get(buffer);
+
+        listener.orderExecuted(orderExecuted);
+    }
+
+    private void orderCanceled(ByteBuffer buffer) throws IOException {
+        orderCanceled.get(buffer);
+
+        listener.orderCanceled(orderCanceled);
+    }
+
+    private void orderDeleted(ByteBuffer buffer) throws IOException {
+        orderDeleted.get(buffer);
+
+        listener.orderDeleted(orderDeleted);
+    }
+
+    private void brokenTrade(ByteBuffer buffer) throws IOException {
+        brokenTrade.get(buffer);
+
+        listener.brokenTrade(brokenTrade);
+    }
+
+}
