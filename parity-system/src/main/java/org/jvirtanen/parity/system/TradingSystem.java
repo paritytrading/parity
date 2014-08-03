75fb1a7d67c6b08999ad897288a38eecdd9355b5
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2014-08-03 21:39:01 +0300

    Add market data to trading system
    For now, the trading system just sends the initial Version message.

--- parity-system/src/main/java/org/jvirtanen/parity/system/TradingSystem.java
+++ parity-system/src/main/java/org/jvirtanen/parity/system/TradingSystem.java
@@ -6,8 +6,11 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import org.joda.time.LocalDate;
 import org.jvirtanen.config.Configs;
+import org.jvirtanen.parity.net.pmd.PMD;
 
 class TradingSystem {
 
@@ -27,9 +30,21 @@ class TradingSystem {
     }
 
     private static void main(Config config) throws IOException {
+        String      marketDataSession        = config.getString("market-data.session");
+        InetAddress marketDataMulticastGroup = Configs.getInetAddress(config, "market-data.multicast-group");
+        int         marketDataMulticastPort  = Configs.getPort(config, "market-data.multicast-port");
+
+        MarketData marketData = MarketData.create(marketDataSession,
+                new InetSocketAddress(marketDataMulticastGroup, marketDataMulticastPort));
+
         int orderEntryPort = Configs.getPort(config, "order-entry.port");
 
         OrderEntry orderEntry = OrderEntry.create(orderEntryPort);
+
+        PMD.Version message = new PMD.Version();
+        message.version = PMD.VERSION;
+
+        marketData.send(message);
 
         orderEntry.run();
     }
