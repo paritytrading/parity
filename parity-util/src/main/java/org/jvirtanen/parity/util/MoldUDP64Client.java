d6976cccb59a39b12c82da31dbb1b022dad67f8c
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2015-04-15 21:06:06 +0300

    Switch from single-channel to multi-channel MoldUDP64 client
    The single-channel MoldUDP64 client uses the same channel for
    receiving downstream packets and sending request packets. This
    results in the source UDP port in request packets being equal
    to the UDP port of the IP multicast. Thus, only one instance
    of this implementation can be listening to a particular session
    on a particular host at a time.
    
    Switch to the multi-channel MoldUDP64 client. It uses separate
    channels for receiving downstream packets and sending request
    packets.

--- parity-util/src/main/java/org/jvirtanen/parity/util/MoldUDP64Client.java
+++ parity-util/src/main/java/org/jvirtanen/parity/util/MoldUDP64Client.java
@@ -7,19 +7,30 @@ import java.net.NetworkInterface;
 import java.net.StandardProtocolFamily;
 import java.net.StandardSocketOptions;
 import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import org.jvirtanen.nassau.MessageListener;
 import org.jvirtanen.nassau.moldudp64.MoldUDP64ClientState;
 import org.jvirtanen.nassau.moldudp64.MoldUDP64ClientStatusListener;
-import org.jvirtanen.nassau.moldudp64.SingleChannelMoldUDP64Client;
+import org.jvirtanen.nassau.moldudp64.MultiChannelMoldUDP64Client;
 
 /**
  * This class implements a MoldUDP64 client.
  */
 public class MoldUDP64Client implements Closeable {
 
-    private SingleChannelMoldUDP64Client backing;
+    private Selector selector;
 
-    private MoldUDP64Client(SingleChannelMoldUDP64Client backing) {
+    private SelectionKey requestChannelKey;
+
+    private MultiChannelMoldUDP64Client backing;
+
+    private MoldUDP64Client(Selector selector, SelectionKey requestChannelKey,
+            MultiChannelMoldUDP64Client backing) {
+        this.selector = selector;
+
+        this.requestChannelKey = requestChannelKey;
+
         this.backing = backing;
     }
 
@@ -41,6 +52,12 @@ public class MoldUDP64Client implements Closeable {
         channel.setOption(StandardSocketOptions.SO_REUSEADDR, true);
         channel.bind(new InetSocketAddress(multicastGroup.getPort()));
         channel.join(multicastGroup.getAddress(), multicastInterface);
+        channel.configureBlocking(false);
+
+        DatagramChannel requestChannel = DatagramChannel.open(StandardProtocolFamily.INET);
+
+        requestChannel.connect(requestAddress);
+        requestChannel.configureBlocking(false);
 
         MoldUDP64ClientStatusListener statusListener = new MoldUDP64ClientStatusListener() {
 
@@ -62,23 +79,44 @@ public class MoldUDP64Client implements Closeable {
 
         };
 
-        SingleChannelMoldUDP64Client backing = new SingleChannelMoldUDP64Client(channel,
-                requestAddress, listener, statusListener);
+        Selector selector = Selector.open();
 
-        return new MoldUDP64Client(backing);
+        channel.register(selector, SelectionKey.OP_READ, null);
+
+        SelectionKey requestChannelKey = requestChannel.register(selector,
+                SelectionKey.OP_READ, null);
+
+        MultiChannelMoldUDP64Client backing = new MultiChannelMoldUDP64Client(channel,
+                requestChannel, listener, statusListener);
+
+        return new MoldUDP64Client(selector, requestChannelKey, backing);
     }
 
     /**
-     * Receive a downstream packet.
+     * Receive data.
      *
      * @throws IOException if an I/O error occurs
      */
     public void receive() throws IOException {
-        backing.receive();
+        while (selector.select() == 0);
+
+        if (requestChannelKey.isReadable()) {
+            backing.receiveResponse();
+
+            selector.selectedKeys().remove(requestChannelKey);
+        }
+
+        if (selector.selectedKeys().size() > 0) {
+            backing.receive();
+
+            selector.selectedKeys().clear();
+        }
     }
 
     @Override
     public void close() throws IOException {
+        selector.close();
+
         backing.close();
     }
 
