611ac0dce521b66daaff200ab78aae635c3c812a
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2015-05-13 14:59:34 +0300

    Add documentation for PMR exception in network protocols

--- parity-net/src/main/java/org/jvirtanen/parity/net/pmr/PMRException.java
+++ parity-net/src/main/java/org/jvirtanen/parity/net/pmr/PMRException.java
@@ -2,8 +2,16 @@ package org.jvirtanen.parity.net.pmr;
 
 import org.jvirtanen.parity.net.ProtocolException;
 
+/**
+ * Indicates a protocol error while handling the PMR protocol.
+ */
 public class PMRException extends ProtocolException {
 
+    /**
+     * Construct an instance with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public PMRException(String message) {
         super(message);
     }
