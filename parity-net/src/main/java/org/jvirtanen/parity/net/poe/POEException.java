8b16b1f73dfd797a0af431ec9bce4ec7df72c757
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2015-05-13 14:59:54 +0300

    Add documentation for POE exception in network protocols

--- parity-net/src/main/java/org/jvirtanen/parity/net/poe/POEException.java
+++ parity-net/src/main/java/org/jvirtanen/parity/net/poe/POEException.java
@@ -2,8 +2,16 @@ package org.jvirtanen.parity.net.poe;
 
 import org.jvirtanen.parity.net.ProtocolException;
 
+/**
+ * Indicates a protocol error while handling the POE protocol.
+ */
 public class POEException extends ProtocolException {
 
+    /**
+     * Construct an instance with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public POEException(String message) {
         super(message);
     }
