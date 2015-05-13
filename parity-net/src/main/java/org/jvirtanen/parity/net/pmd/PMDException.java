337aa7f2aa4893e055674230d5fcaa23eb0ffdb2
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2015-05-13 14:59:12 +0300

    Add documentation for PMD exception in network protocols

--- parity-net/src/main/java/org/jvirtanen/parity/net/pmd/PMDException.java
+++ parity-net/src/main/java/org/jvirtanen/parity/net/pmd/PMDException.java
@@ -2,8 +2,16 @@ package org.jvirtanen.parity.net.pmd;
 
 import org.jvirtanen.parity.net.ProtocolException;
 
+/**
+ * Indicates a protocol error while handling the PMD protocol.
+ */
 public class PMDException extends ProtocolException {
 
+    /**
+     * Construct an instance with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public PMDException(String message) {
         super(message);
     }
