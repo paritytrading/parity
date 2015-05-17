832f8d73b35a7b422b7fbf2bbd3d0e0aa488d5be
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2015-05-17 20:27:07 +0300

    Add TSV support to trade reporter

--- parity-reporter/src/main/java/org/jvirtanen/parity/reporter/DisplayFormat.java
+++ parity-reporter/src/main/java/org/jvirtanen/parity/reporter/DisplayFormat.java
@@ -2,27 +2,17 @@ package org.jvirtanen.parity.reporter;
 
 import static org.jvirtanen.lang.Strings.*;
 
-import java.util.Locale;
 import org.jvirtanen.parity.net.pmr.PMR;
-import org.jvirtanen.parity.net.pmr.PMRListener;
 import org.jvirtanen.parity.util.Timestamps;
 
-class Display implements PMRListener {
-
-    private static final double PRICE_FACTOR = 10000.0;
-
-    private static final long NANOS_PER_MILLI = 1000 * 1000;
+class DisplayFormat extends MarketReportListener {
 
     private static final String HEADER = "" +
         "Timestamp    Inst     Quantity   Price     Buyer    Seller\n" +
         "------------ -------- ---------- --------- -------- --------";
 
-    public Display() {
+    public DisplayFormat() {
         printf("\n%s\n", HEADER);
-    }
-
-    @Override
-    public void order(PMR.Order message) {
     }
 
     @Override
@@ -30,10 +20,6 @@ class Display implements PMRListener {
         printf("%12s %8s %10d %9.2f %8s %8s\n", Timestamps.format(message.timestamp / NANOS_PER_MILLI),
                 decodeLong(message.instrument), message.quantity, message.price / PRICE_FACTOR,
                 decodeLong(message.buyer), decodeLong(message.seller));
-    }
-
-    private void printf(String format, Object... args) {
-        System.out.printf(Locale.US, format, args);
     }
 
 }
