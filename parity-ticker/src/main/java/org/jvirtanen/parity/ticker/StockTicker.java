65403781654e116cd984225d4b4357d6423be35b
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2015-11-19 23:04:04 +0200

    Add SoupBinTCP support to stock ticker

--- parity-ticker/src/main/java/org/jvirtanen/parity/ticker/StockTicker.java
+++ parity-ticker/src/main/java/org/jvirtanen/parity/ticker/StockTicker.java
@@ -12,9 +12,11 @@ import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
 import java.util.List;
 import org.jvirtanen.config.Configs;
+import org.jvirtanen.nassau.MessageListener;
 import org.jvirtanen.parity.net.pmd.PMDParser;
 import org.jvirtanen.parity.top.Market;
 import org.jvirtanen.parity.util.MoldUDP64;
+import org.jvirtanen.parity.util.SoupBinTCP;
 
 class StockTicker {
 
@@ -43,12 +45,6 @@ class StockTicker {
     }
 
     private static void main(Config config, boolean taq) throws IOException {
-        NetworkInterface multicastInterface = Configs.getNetworkInterface(config, "market-data.multicast-interface");
-        InetAddress      multicastGroup     = Configs.getInetAddress(config, "market-data.multicast-group");
-        int              multicastPort      = Configs.getPort(config, "market-data.multicast-port");
-        InetAddress      requestAddress     = Configs.getInetAddress(config, "market-data.request-address");
-        int              requestPort        = Configs.getPort(config, "market-data.request-port");
-
         List<String> instruments = config.getStringList("instruments");
 
         MarketDataListener listener = taq ? new TAQFormat() : new DisplayFormat(instruments);
@@ -60,8 +56,27 @@ class StockTicker {
 
         MarketDataProcessor processor = new MarketDataProcessor(market, listener);
 
-        MoldUDP64.receive(multicastInterface, new InetSocketAddress(multicastGroup, multicastPort),
-                new InetSocketAddress(requestAddress, requestPort), new PMDParser(processor));
+        main(config, new PMDParser(processor));
+    }
+
+    private static void main(Config config, MessageListener listener) throws IOException {
+        if (config.hasPath("market-data.multicast-interface")) {
+            NetworkInterface multicastInterface = Configs.getNetworkInterface(config, "market-data.multicast-interface");
+            InetAddress      multicastGroup     = Configs.getInetAddress(config, "market-data.multicast-group");
+            int              multicastPort      = Configs.getPort(config, "market-data.multicast-port");
+            InetAddress      requestAddress     = Configs.getInetAddress(config, "market-data.request-address");
+            int              requestPort        = Configs.getPort(config, "market-data.request-port");
+
+            MoldUDP64.receive(multicastInterface, new InetSocketAddress(multicastGroup, multicastPort),
+                    new InetSocketAddress(requestAddress, requestPort), listener);
+        } else {
+            InetAddress address  = Configs.getInetAddress(config, "market-data.address");
+            int         port     = Configs.getPort(config, "market-data.port");
+            String      username = config.getString("market-data.username");
+            String      password = config.getString("market-data.password");
+
+            SoupBinTCP.receive(new InetSocketAddress(address, port), username, password, listener);
+        }
     }
 
 }
