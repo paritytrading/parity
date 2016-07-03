2415b698c2bb483fcf2b8426152f7104611e3f8b
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2016-07-03 13:29:40 +0300

    Reduce measurement iterations in matching engine performance test

--- parity-match-perf-test/src/main/java/com/paritytrading/parity/match/perf/MarketBenchmark.java
+++ parity-match-perf-test/src/main/java/com/paritytrading/parity/match/perf/MarketBenchmark.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
@@ -15,6 +16,7 @@ import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
 @State(Scope.Benchmark)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @BenchmarkMode(Mode.SampleTime)
