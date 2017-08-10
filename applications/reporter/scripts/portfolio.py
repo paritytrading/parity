48c63bd21b3eafbe74e6b0e84caf1b990467e102
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2017-08-10 23:34:59 +0200

    Update portfolio script in trade reporter
    Make the portfolio script work with configurable price and size format.

--- applications/reporter/scripts/portfolio.py
+++ applications/reporter/scripts/portfolio.py
@@ -26,7 +26,7 @@ class User(object):
 
 def record(timestamp, username):
     user = users[username]
-    cash = '{:.2f}'.format(user.cash)
+    cash = '{}'.format(user.cash)
     inventory = ['{}:{}'.format(instrument, quantity) for instrument, quantity
             in user.inventory.iteritems()]
     print '\t'.join([timestamp, username, cash, ','.join(sorted(inventory))])
@@ -40,12 +40,12 @@ for row in csv.DictReader(iter(sys.stdin.readline, None), delimiter='\t'):
     timestamp = row['Timestamp']
     instrument = row['Instrument']
     price = float(row['Price'])
-    quantity = int(row['Quantity'])
+    quantity = float(row['Quantity'])
     seller = users[row['Seller']]
     buyer = users[row['Buyer']]
     seller.cash += price * quantity
     buyer.cash -= price * quantity
-    seller.inventory[instrument] = seller.inventory.get(instrument, 0) - quantity
-    buyer.inventory[instrument] = buyer.inventory.get(instrument, 0) + quantity
+    seller.inventory[instrument] = seller.inventory.get(instrument, 0.0) - quantity
+    buyer.inventory[instrument] = buyer.inventory.get(instrument, 0.0) + quantity
     record(timestamp, row['Seller'])
     record(timestamp, row['Buyer'])
