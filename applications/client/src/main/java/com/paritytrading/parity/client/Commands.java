3533373791523a9dbc989d80735c3b42ef123927
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2019-01-07 10:18:39 +0200

    parity-client: Clean up 'Commands'
    Remove superfluous 'public' modifiers.

--- applications/client/src/main/java/com/paritytrading/parity/client/Commands.java
+++ applications/client/src/main/java/com/paritytrading/parity/client/Commands.java
@@ -3,7 +3,7 @@ package com.paritytrading.parity.client;
 import com.paritytrading.parity.net.poe.POE;
 import java.util.stream.Stream;
 
-public class Commands {
+class Commands {
 
     private static final Command[] COMMANDS = new Command[] {
         new EnterCommand(POE.BUY),
@@ -16,21 +16,18 @@ public class Commands {
         new ExitCommand(),
     };
 
-    private Commands() {
-    }
-
-    public static Command[] all() {
+    static Command[] all() {
         return COMMANDS;
     }
 
-    public static Command find(final String name) {
+    static Command find(final String name) {
         return Stream.of(COMMANDS)
                 .filter(c -> c.getName().equals(name))
                 .findFirst()
                 .orElse(null);
     }
 
-    public static String[] names() {
+    static String[] names() {
         return Stream.of(COMMANDS).map(Command::getName).toArray(String[]::new);
     }
 
