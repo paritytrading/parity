25c49fbef10f36176193f0d676d558f656461841
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2019-02-19 22:59:19 +0000

    parity-reporter: Clean up 'TradeListener'
    Remote superfluous 'public' modifiers.

--- applications/reporter/src/main/java/com/paritytrading/parity/reporter/TradeListener.java
+++ applications/reporter/src/main/java/com/paritytrading/parity/reporter/TradeListener.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 abstract class TradeListener {
 
-    public abstract void trade(Trade event);
+    abstract void trade(Trade event);
 
     protected void printf(String format, Object... args) {
         System.out.printf(Locale.US, format, args);
