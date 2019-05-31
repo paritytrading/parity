ab8cee1bea55155aaa7de989f0f41e706f86cd57
Jan Nielsen <jan.sture.nielsen@gmail.com>
2019-05-31 14:33:31 -0600

    Handle missing message type gracefully.

--- libraries/net/src/main/java/com/paritytrading/parity/net/poe/POEClientParser.java
+++ libraries/net/src/main/java/com/paritytrading/parity/net/poe/POEClientParser.java
@@ -36,6 +36,9 @@ public class POEClientParser implements MessageListener {
     public void message(ByteBuffer buffer) throws IOException {
         int length = buffer.remaining();
 
+        if (length < 1)
+            throw new POEException("Malformed message: no message type");
+
         byte messageType = buffer.get();
 
         switch (messageType) {
