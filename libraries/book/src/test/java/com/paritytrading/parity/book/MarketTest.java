743bacd756e12b39edacaa85d97fb1529731eff5
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2021-03-29 18:04:42 +0300

    parity-book: Clean up test dependencies

--- libraries/book/src/test/java/com/paritytrading/parity/book/MarketTest.java
+++ libraries/book/src/test/java/com/paritytrading/parity/book/MarketTest.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.jvirtanen.value.Value;
 
 class MarketTest {
 
